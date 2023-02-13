06cb6cb475e9866341253f6da4b509386db995ae
Mark Nosal <mark.c.nosal@gmail.com>
2023-02-13 14:21:35 -0600

    Finished the lab!  Super prooud of myself!!

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (i = 0; i < array.length; i++) {
+    const difference = target - array[i]
+      for (j = i + 1; j < array.length; j++) {
+        if (difference === array[j]) {
+          return true
+        }
+      }
+  }
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+
 */
 
 /* 
   Add your pseudocode here
+  forloop
+  Subtract first number in array from the Target number.
+  Difference = target - array[i]
+  Iterate through remaining numbers and return true if any of them equal the Difference.
+  if (difference === array[i]){return true})
+  Do this with remaining numbers in array.
+  another for loop (if (difference === array[i]){return true})
+  If none of the numbers equal the Difference, return false. 
+  return false
+
 */
 
 /*
   Add written explanation of your solution here
+  Subtract first number in array from the Target number.
+  Iterate through remaining numbers and return true if any of them equal the Difference.
+  Do this with remaining numbers in array.
+  If none of the numbers equal the Difference, return false. 
 */
 
 // You can run `node index.js` to view these console logs
